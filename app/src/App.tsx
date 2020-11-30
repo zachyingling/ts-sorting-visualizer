2f07ae777c4a74223576dcc58392579e2b8e6f3c
Zach Yingling <zpyingling9559@gmail.com>
2020-11-29 19:41:51 -0500

    adding sorting algorithms to the app; only merge sort is working ; and adding buttons to header

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -25,6 +25,7 @@ export default class App extends React.Component<IProps, ArrayState> {
     this.bubbleSort = this.bubbleSort.bind(this);
     this.quickSort = this.quickSort.bind(this);
     this.insertionSort = this.insertionSort.bind(this);
+    this.setArray = this.setArray.bind(this);
 
     this.state = {
       firstGenArray: [],
@@ -49,13 +50,121 @@ export default class App extends React.Component<IProps, ArrayState> {
     });
   }
 
-  mergeSort(){}
+  setArray(arr: number[]){
+    return this.setState({
+      firstGenArray: arr
+    });
+  }
 
-  bubbleSort(){}
+  mergeSort(mainArray: number [] = this.state.firstGenArray){
+    if(mainArray.length <= 1) return mainArray;
+    let mid = Math.floor(mainArray.length / 2);
+    let left: number[] = this.mergeSort(mainArray.slice(0, mid));
+    let right: number[] = this.mergeSort(mainArray.slice(mid));
+    let tempArr: number[] = this.merge(left, right); 
+    return tempArr;
+  }
 
-  quickSort(){}
+  // method to merge two sorted arrays/single elements
+  // used in merge sorting algo
+  merge(arr1: number[], arr2: number[]){
+    let results = [];
+    let i = 0;
+    let j = 0;
 
-  insertionSort(){}
+    while(i < arr1.length && j < arr2.length){
+      if(arr2[j] > arr1[i]){
+        results.push(arr1[i]);
+        i++;
+      } else {
+        results.push(arr2[j])
+        j++;
+      }
+    }
+
+    while(i < arr1.length) {
+      results.push(arr1[i])
+      i++;
+    }
+
+    while(j < arr2.length) {
+      results.push(arr2[j])
+      j++;
+    }
+
+    return results;
+  }
+
+  bubbleSort(arr: number[]){
+    let noSwaps;
+    for(let i = arr.length; i > 0; i--){
+      noSwaps = true;
+      for(let j = 0; j < i - 1; j++){
+        if(arr[j] > arr[j + 1]){
+          this.swap(arr, j, j+1);
+          noSwaps = false;
+        }
+      }
+      if(noSwaps) break;
+    }
+    return arr;
+  }
+
+  swap(arr: number[], index1: number, index2: number){
+    var temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+  }
+
+  quickSort(arr: number[], left = 0, right = arr.length - 1){
+    if(left < right) { 
+      let pivotIndex = this.pivot(arr, left, right);
+      // left
+      // in the call stack the left side of the pivot will finish first
+      this.quickSort(arr, left, pivotIndex - 1);
+      // right
+      // in the call stack the right side of the pivot will finish after the left side
+      this.quickSort(arr, pivotIndex + 1, right);
+    }
+    return arr;
+  }
+
+  pivot(arr: number[], start = 0, end = arr.length - 1){
+    // ES2015 Syntax Swap Function
+    // Takes an array, and two indeces
+    // Just swaps the two values at the two given indeces in the same array
+    const swap = (arr: number[], idx1: number, idx2: number) => {
+      [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+    };
+  
+    // We are assuming the pivot is always the first element
+    let pivot = arr[start];
+    let swapIdx = start;
+  
+    for (let i = start + 1; i <= end; i++) {
+      if (pivot > arr[i]) {
+        swapIdx++;
+        swap(arr, swapIdx, i);
+      }
+    }
+  
+    // Swap the pivot from the start to the swapIndex
+    swap(arr, start, swapIdx);
+    return swapIdx;
+  }
+
+  insertionSort(arr: number[]){
+    for(let i = 1; i < arr.length; i++){
+      let currentVal = arr[i];
+      let j;
+  
+      for(j = i - 1; j >= 0 && arr[j] > currentVal; j--){
+        arr[j+1] = arr[j];
+      }
+      arr[j + 1] = currentVal;
+    }
+    return arr;
+  }
 
   render(){
     return (
@@ -67,6 +176,7 @@ export default class App extends React.Component<IProps, ArrayState> {
             bubbleSort={this.bubbleSort}
             quickSort={this.quickSort}
             insertionSort={this.insertionSort}
+            setArray={this.setArray}
           />
           <Container fluid>
             <Row className="justify-content-md-center">
--- app/src/Components/Header/Header.tsx
+++ app/src/Components/Header/Header.tsx
@@ -3,10 +3,11 @@ import "./Header.css";
 
 interface IProps {
   generateArray: () => void,
-  mergeSort: () => void,
-  bubbleSort: () => void,
-  quickSort: () => void,
-  insertionSort: () => void
+  mergeSort: (mainArray: number[]) => number[],
+  bubbleSort: (arr: number[]) => number[],
+  quickSort: (arr: number[], left: number, right: number) => number[],
+  insertionSort: (arr: number[]) => number[],
+  setArray: (arr: number[]) => void
 }
 
 export default class Header extends React.Component<IProps> {
@@ -14,10 +15,13 @@ export default class Header extends React.Component<IProps> {
     return (
       <header>
         <h1 id="title">Sorting Visualizer</h1>
-        <button onClick={this.props.mergeSort}>Merge Sort</button>
-        <button onClick={this.props.bubbleSort}>Bubble Sort</button>
-        <button onClick={this.props.quickSort}>Quick Sort</button>
-        <button onClick={this.props.insertionSort}>Insertion Sort</button>
+        <button onClick={(event: React.MouseEvent<HTMLElement>) => {
+          let arr = this.props.mergeSort((event.target as any).index);
+          this.props.setArray(arr);
+        }}>Merge Sort</button>
+        <button onClick={() => this.props.bubbleSort}>Bubble Sort</button>
+        <button onClick={() => this.props.quickSort}>Quick Sort</button>
+        <button onClick={() => this.props.insertionSort}>Insertion Sort</button>
         <button onClick={this.props.generateArray}>Generate A New Array</button>
       </header>
     );
